52feb2ecdb51587dd7bd5be508dfc82d3360d09c
sann <psang4845@gmail.com>
2023-07-24 01:22:11 +0700

    Update Feature/EditLayout

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -1,4 +1,31 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
 
-// Write your JavaScript code.
+var handleSearchPost = () => {
+    var searchResultsDiv = document.getElementById("searchResults");
+    var searchTerm = document.getElementById("searchTerm").value;
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/Home/Search?searchTerm=" + searchTerm, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            searchResultsDiv.innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+}
+
+var handlePagingPost = (page) => {
+    var pagingResultsDiv = document.getElementById("pagingResults");
+    console.log(pagingResultsDiv);
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/Home/Paging?page=" + page, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+            pagingResultsDiv.innerHTML = xhr.responseText;
+        }
+    };
+    xhr.send();
+}
+
+window.onload = () => {
+    handlePagingPost();
+};
